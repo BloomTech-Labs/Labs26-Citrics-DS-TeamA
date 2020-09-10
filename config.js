76b39b4276687e53ce7d4ea468ff023bcc2cc56f
Samuel <samswank@gmail.com>
2020-09-10 15:05:18 -0600

    dynamic deployment solved

--- config.js
+++ config.js
@@ -1,7 +1,39 @@
-const DOCKER_ID = "shengjiyang"
-const DOCKER_IMAGE = "labs26-citrics-ds-teama_web"
-const DOCKER_IMAGE_TAG = "latest"
+const Dockerrun = require('./Dockerrun.aws.json')
+const fs = require('fs');
 
-module.exports.DOCKER_ID = DOCKER_ID
-module.exports.DOCKER_IMAGE = DOCKER_IMAGE
-module.exports.DOCKER_IMAGE = DOCKER_IMAGE_TAG
+var env = require('./env');
+
+// Function for Reading the .json file
+function jsonReader(filePath, cb) {
+    fs.readFile(filePath, 'utf-8', (err, fileData) => {
+        if (err) {
+            return cb && cb(err)
+        }
+        try {
+            const object = JSON.parse(fileData);
+            return cb && cb(null, object);
+        } catch (err) {
+            return cb && cb(err);
+        }
+    });
+}
+
+const fileName = './Dockerrun.aws.json';
+const file = require(fileName);
+
+file.Image.Name = env.DOCKER_ID + "/" + env.DOCKER_IMAGE + ":" + env.DOCKER_IMAGE_TAG
+
+// Function for overwriting data in the .json file
+fs.writeFile(fileName, JSON.stringify(file, null, 2), function writeJSON(err) {
+  if (err) return console.log(err);
+  console.log(JSON.stringify(file));
+  console.log('writing to ' + fileName);
+});
+
+jsonReader('./Dockerrun.aws.json', (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data)
+    }
+});
