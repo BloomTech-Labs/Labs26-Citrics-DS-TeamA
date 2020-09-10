5634135d9d18d05b1882869313efec133b0c3cc4
Samuel <samswank@gmail.com>
2020-09-10 13:33:55 -0600

    added node js env file

---
+++ env.js
@@ -0,0 +1,7 @@
+const DOCKER_ID = "shengjiyang"
+const DOCKER_IMAGE = "labs26-citrics-ds-teama_web"
+const DOCKER_IMAGE_TAG = "latest"
+
+module.exports.DOCKER_ID = DOCKER_ID
+module.exports.DOCKER_IMAGE = DOCKER_IMAGE
+module.exports.DOCKER_IMAGE = DOCKER_IMAGE_TAG
