186ae9486b4b5948d726368f70c06bed0fe0a0b1
Samuel <samswank@gmail.com>
2020-09-14 11:17:00 -0600

    pretty print lexicon

---
+++ data/weather/dictionary.py
@@ -0,0 +1,49 @@
+import os
+import json
+
+lexicon = open(os.path.join("data", "weather", "lexicon.txt"), "r")
+
+list_ = []
+for line in lexicon:
+    stripped_line = line.strip()
+    line_list = stripped_line.split()
+    list_.append(line_list)
+
+zipcodes = [list_[i][0] for i in range(len(list_))]
+cities = [" ".join(list_[i][2:]) for i in range(len(list_))]
+
+by_zip = {}
+
+for i in range(len(zipcodes)):
+    by_zip[zipcodes[i]] = cities[i]
+
+by_city= {}
+
+for i in range(len(zipcodes)):
+    by_city[cities[i]] = zipcodes[i]
+
+by_zip_tup = sorted(list(by_zip.items()), key=lambda x:x[0])
+by_city_tup = sorted(list(by_city.items()), key=lambda x:x[0])
+by_state_tup = sorted(list(by_city.items()), key=lambda x:x[0][-2:])
+
+by_zip = {key:val for (key, val) in by_zip_tup}
+by_city = {key:val for (key, val) in by_city_tup}
+by_state = {key:val for (key, val) in by_state_tup}
+
+by_zip_json = json.dumps(by_zip, indent=2)
+by_city_json = json.dumps(by_city, indent=2)
+by_state_json = json.dumps(by_state, indent=2)
+
+print("By Zipcode")
+print("----------")
+print(by_zip_json)
+print("")
+
+print("By City")
+print("-------")
+print(by_city_json)
+print("")
+
+print("By State")
+print("--------")
+print(by_state_json)
