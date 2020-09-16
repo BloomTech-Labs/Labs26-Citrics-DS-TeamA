7d67863fd478dae4f401d66486e68c21a9affca9
Aaron Watkins <61791690+ekselan@users.noreply.github.com>
2020-09-16 18:29:10 -0400

    pulling from master branch

--- project/app/main.py
+++ project/app/main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
-from app.api import predict, viz, rental1, walkability, current
+from app.api import predict, viz, rental1, walkability, current, rentviz1, rentviz_view, rent_city_states
+
 
 # Description Text
 DESC_TEXT = "Finding a place to live is hard! Nomads struggle with finding the right city for them. Citrics is a city comparison tool that allows users to compare cities and find cities based on user preferences."
@@ -20,6 +21,8 @@ app.include_router(rent_city_states.router)
 app.include_router(rental1.router)
 app.include_router(walkability.router)
 app.include_router(current.router)
+app.include_router(rentviz1.router)
+app.include_router(rentviz_view.router)
 
 
 app.add_middleware(
