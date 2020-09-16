1ce41d7788543b2bf858b7b35f6ec1220c7e3352
Aaron Watkins <61791690+ekselan@users.noreply.github.com>
2020-09-16 16:58:56 -0400

    add endpoint for citynames and state codes (from rental price data) per request from F.E. team

---
+++ project/app/api/rent_city_states.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter, HTTPException
+from app.sql_query_function import fetch_query
+
+router = APIRouter()
+
+
+@router.get("/rent_city_state/")
+async def rental():
+    """
+    City-level historic rental prices for 1 bedroom apartments from
+    [Apartment List](https://www.apartmentlist.com/research/category/data-rent-estimates) 📈
+
+    ### Response
+    JSON string of city names and statecodes found in the rental price data.
+    """
+
+    query = """
+    SELECT
+        city,
+        "state"
+    FROM rp_clean1
+    WHERE bedroom_size = 'Studio'
+    """
+
+    columns = ["city", "state"]
+
+    return fetch_query(query, columns)
