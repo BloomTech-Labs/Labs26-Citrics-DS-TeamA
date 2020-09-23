2b7d1fea264216ec25b090b303ad02bb80c6e393
Samuel <samswank@gmail.com>
2020-09-23 17:06:21 -0600

    rental_pred sql call

--- project/app/api/rental_pred.py
+++ project/app/api/rental_pred.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
+from dotenv import load_dotenv
 import psycopg2
 import os
-from dotenv import load_dotenv
 import warnings
 import pandas as pd
 from statsmodels.tsa.holtwinters import ExponentialSmoothing
@@ -9,6 +9,22 @@ from statsmodels.tsa.holtwinters import ExponentialSmoothing
 router = APIRouter()
 
 @router.get("/rental/predict/{city}_{state}")
+load_dotenv()
+
+DB_NAME = os.getenv("DB_NAME", "Invalid DB_NAME")
+DB_USER = os.getenv("DB_USER", "Invalid DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "Invalid DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST", "Invalid DB_HOST")
+
+connection = psycopg2.connect(
+    dbname=DB_NAME,
+    user=DB_USER,
+    password=DB_PASSWORD,
+    host=DB_HOST
+    )
+
+cur = connection.cursor()
+
 def rental_predictions(city, state):
     warnings.filterwarnings("ignore", message="After 0.13 initialization must be handled at model creation")
     query = """
