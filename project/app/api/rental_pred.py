5a3b07442e57548481f937148b9fa778ef922356
Aaron Watkins <61791690+ekselan@users.noreply.github.com>
2020-09-23 21:00:11 -0400

    deploy updates to fastapi

--- project/app/api/rental_pred.py
+++ project/app/api/rental_pred.py
@@ -8,41 +8,42 @@ from statsmodels.tsa.holtwinters import ExponentialSmoothing
 router = APIRouter()
 
 @router.get("/rental/predict/{city}_{state}")
+async def pred(city: str, state: str):
 
-DB_USER = "citrics"
-DB_PASSWORD = "BnDW2WupbFpgZSewsZm7"
-DB_NAME = "postgres"
-DB_HOST = "citricsads.cav8gkdxva9e.us-east-1.rds.amazonaws.com"
+    DB_USER = "citrics"
+    DB_PASSWORD = "BnDW2WupbFpgZSewsZm7"
+    DB_NAME = "postgres"
+    DB_HOST = "citricsads.cav8gkdxva9e.us-east-1.rds.amazonaws.com"
 
-connection = psycopg2.connect(
-    dbname=DB_NAME,
-    user=DB_USER,
-    password=DB_PASSWORD,
-    host=DB_HOST
-    )
+    connection = psycopg2.connect(
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST
+        )
 
-cur = connection.cursor()
+    cur = connection.cursor()
 
-def rental_predictions(city, state):
-    warnings.filterwarnings("ignore", message="After 0.13 initialization must be handled at model creation")
-    query = """
-    SELECT "month", "Studio", "onebr", "twobr", "threebr", "fourbr"
-    FROM rental
-    WHERE "city"='{city}' and "state"='{state}';
-    """.format(city=city, state=state)
+    def rental_predictions(city, state):
+        warnings.filterwarnings("ignore", message="After 0.13 initialization must be handled at model creation")
+        query = """
+        SELECT "month", "Studio", "onebr", "twobr", "threebr", "fourbr"
+        FROM rental
+        WHERE "city"='{city}' and "state"='{state}';
+        """.format(city=city, state=state)
 
-    cur.execute(query)
+        cur.execute(query)
 
-    df =  pd.DataFrame.from_records(cur.fetchall(), columns=["month", "Studio", "onebr", "twobr", "threebr", "fourbr"])
-    df.set_index("month", inplace=True)
-    df.index = pd.to_datetime(df.index)
-    df.index.freq = "MS"
-    
-    series = []
+        df =  pd.DataFrame.from_records(cur.fetchall(), columns=["month", "Studio", "onebr", "twobr", "threebr", "fourbr"])
+        df.set_index("month", inplace=True)
+        df.index = pd.to_datetime(df.index)
+        df.index.freq = "MS"
+        
+        series = []
 
-    for col in df.columns:
-        s = ExponentialSmoothing(df["2014-06-01":]["Studio"], trend="add", seasonal="add", seasonal_periods=12).fit().forecast(12)
-        s.name = col
-        series.append(s)
+        for col in df.columns:
+            s = ExponentialSmoothing(df["2014-06-01":]["Studio"], trend="add", seasonal="add", seasonal_periods=12).fit().forecast(12)
+            s.name = col
+            series.append(s)
 
-    return pd.concat(series, axis=1).to_json(indent=2)
+        return pd.concat(series, axis=1).to_json(indent=2)
