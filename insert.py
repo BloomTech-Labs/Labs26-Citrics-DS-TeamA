fe934520fed336b876aeca27019106cf50849271
Samuel <samswank@gmail.com>
2020-09-28 13:52:28 -0600

    reset city added

--- insert.py
+++ insert.py
@@ -25,10 +25,9 @@ connection = psycopg2.connect(
 
 cur = connection.cursor()
 
-def insert_csv(city: str, state: str):
+def insert_csv(city=None, state=None, filepath=None):
     create_table = """
     CREATE TABLE IF NOT EXISTS historic_weather(
-        ID SERIAL PRIMARY KEY,
         date_time TIMESTAMP NOT NULL,
         location INT NOT NULL,
         city varchar(20) NOT NULL,
@@ -45,14 +44,20 @@ def insert_csv(city: str, state: str):
     cur.execute(create_table)
     connection.commit()
 
-    df = pd.read_csv(os.path.join("data", "weather", f"{city}_{state}.csv"))
-    df = df[["date_time", "location", "tempC", "FeelsLikeC", "precipMM", "totalSnow_cm", "humidity", "pressure"]]
-    df.insert(2, "city", [city.title()] * len(df.index))
-    df.insert(3, "state", [state.upper()] * len(df.index))
+    if city and state:
+        df = pd.read_csv(os.path.join("data", "weather", f"{city}_{state}.csv"))
+        df = df[["date_time", "location", "tempC", "FeelsLikeC", "precipMM", "totalSnow_cm", "humidity", "pressure"]]
+        df.insert(2, "city", [city.title()] * len(df.index))
+        df.insert(3, "state", [state.upper()] * len(df.index))
+
+    elif filepath:
+        df = pd.read_csv(os.path.join("data", "weather", filepath))
+        df = df[["date_time", "location", "tempC", "FeelsLikeC", "precipMM", "totalSnow_cm", "humidity", "pressure"]]
+        df.insert(2, "city", [filepath[:-7].title()] * len(df.index))
+        df.insert(3, "state", [filepath[-6:-4].upper()] * len(df.index))
 
     insert_data = """
     INSERT INTO historic_weather(
-        ID, 
         date_time,
         location,
         city,
@@ -66,8 +71,59 @@ def insert_csv(city: str, state: str):
     ) VALUES%s
     """
 
-    execute_values(cur, insert_data, list(df.to_records(index=True)))
+    execute_values(cur, insert_data, list(df.to_records(index=False)))
+    connection.commit()
+
+def reset():
+    reset_table = """
+    DROP TABLE historic_weather;
+    """
+    cur.execute(reset_table)
+    connection.commit()
+
+def reset_city(city, state):
+    delete = """
+    DELETE FROM historic_weather
+    WHERE "city"='{city}' and "state"='{state}'
+    """.format(city=city.title(), state=state.upper())
+
+    cur.execute(delete)
     connection.commit()
 
 if __name__ == "__main__":
-    insert_csv("albuquerque", "nm")
+    input1 = input("""
+    Welcome to the Historical Weather Database Insertion Utility!\n
+    If you would like to insert a city, simply type 'insert' in the prompt
+    below, then type the city name and state abbreviation when prompted.
+
+    If you would like to repopulate the database with all the historic data
+    found in the data/weather directory, type 'populate'
+
+    If you would like to reset the entire Historic Weather Database, simply
+    type 'reset'.
+
+    If you would like to reset only those data for a single city, type
+    'reset city', then type the desired city name and state abbreviation
+    when prompted.
+    """)
+
+    if input1 == "reset":
+        input2 = input("City: ")
+        input3 = input("State: ")
+        remove_city(input2, input3)
+
+    elif input1 == "reset city":
+        input2 = 
+
+    elif input1 == "populate":
+        csv_files = [f for f in os.listdir(os.path.join("data", "weather")) if f[-3:]  == "csv"]
+        for f in csv_files:
+            insert_csv(filepath=f)
+
+    elif input1 == "exit" or input1 == "quit" or input1 == "q":
+        sys.exit()
+
+    else:
+        input2 = input("City: ")
+        input3 = input("State: ")
+        insert_csv(input2, input3)
