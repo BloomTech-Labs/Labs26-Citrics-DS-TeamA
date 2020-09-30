a434a2eaed0d8bbe0afbf7b9a5146336a79c48a0
Samuel <samswank@gmail.com>
2020-09-30 10:40:11 -0600

    weather_pred in db

--- utilities/weather_pred.py
+++ utilities/weather_pred.py
@@ -7,10 +7,14 @@ import pandas as pd
 from insert import retrieve
 import warnings
 from statsmodels.tsa.holtwinters import ExponentialSmoothing
+import datetime
+from psycopg2.extras import execute_values
+
 
 
 register_adapter(np.int64, psycopg2._psycopg.AsIs)
 register_adapter(np.float64, psycopg2._psycopg.AsIs)
+register_adapter(np.datetime64, psycopg2._psycopg.AsIs)
 
 load_dotenv()
 
@@ -28,46 +32,70 @@ connection = psycopg2.connect(
 
 cur = connection.cursor()
 
-def weather_pred(city: str, state: str, statistic: str):
+def weather_pred(city: str, state: str, metric=None):
     df = retrieve(city=city, state=state)
     df.set_index("date_time", inplace=True)
     df.index = pd.to_datetime(df.index)
 
-    if statistic == "min":
-        df = df.resample("MS").min()
+    data = df[metric]
 
-    elif statistic == "mean":
-        df = df.resample("MS").mean()
-
-    elif statistic == "median":
-        df = df.resample("MS").median()
-
-    elif statistic == "max":
-        df = df.resample("MS").max()
-
-    else:
-        print("Exiting program...")
-        sys.exit()
+    raw_series = {
+        "min" : data.resample("MS").min(),
+        "mean" : data.resample("MS").mean(),
+        "median" : data.resample("MS").median(),
+        "max" : data.resample("MS").max()
+    }
 
     warnings.filterwarnings("ignore")
 
     series = []
 
-    for col in df.columns[3:]:
-        s = ExponentialSmoothing(df[col], trend="add", seasonal="add", seasonal_periods=12).fit().forecast(24)
-        s.name = col
+    for d in raw_series.keys():
+        s = ExponentialSmoothing(raw_series[d], trend="add", seasonal="add", seasonal_periods=12).fit().forecast(24)
+        s.name = d
         series.append(s)
 
     result = pd.concat(series, axis=1)
+    result.index = result.index.astype(str)
     result.insert(0, "city", [city] * len(result))
     result.insert(1, "state", [state] * len(result))
 
     for col in result.columns[2:]:
         result.loc[result[col] < 0.0, col] = 0.0
 
-    return result
+    create_table = """
+    CREATE TABLE IF NOT EXISTS {metric}(
+        month TIMESTAMP NOT NULL,
+        city varchar(20) NOT NULL,
+        state varchar(2) NOT NULL,
+        min FLOAT NOT NULL,
+        mean FLOAT NOT NULL,
+        med FLOAT NOT NULL,
+        max FLOAT NOT NULL
+    );
+    """.format(metric=metric)
+
+    cur.execute(create_table)
+    connection.commit()
+
+    insert_data = """
+    INSERT INTO {metric}(
+        month,
+        city,
+        state,
+        min,
+        mean,
+        med,
+        max
+    ) VALUES%s
+    """.format(metric=metric)
+
+    execute_values(cur, insert_data, list(result.to_records(index=True)))
+    connection.commit()
+    # return result.index
 
 
 if __name__ == "__main__":
-    stats = ["min", "mean", "median", "max"]
-    print(weather_pred(city="Salt Lake City", state="UT", statistic="mean"))
+    weather_pred("Salt Lake City", "UT", "tempC")
+    # print(weather_pred("Salt Lake City", "UT", "tempC"))
+    # print(weather_pred("Salt Lake City", "UT", "tempC").index)
