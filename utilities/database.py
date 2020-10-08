a9e5b464fc1ccaf1c9f9384fb27025c863025cde
Samuel Swank <samswank@gmail.com>
2020-10-08 09:42:33 -0600

    create & drop table methods

--- utilities/database.py
+++ utilities/database.py
@@ -13,9 +13,7 @@ class PostgreSQL:
         self.user = os.getenv("DB_USER")
         self.password = os.getenv("DB_PASSWORD")
         self.host = os.getenv("DB_HOST")
-
-    def connection(self):
-        return psycopg2.connect(
+        self.connection = psycopg2.connect(
             dbname=self.name,
             user=self.user,
             password=self.password,
@@ -34,3 +32,36 @@ class PostgreSQL:
 
     def close(self):
         self.connection.close()
+
+    def create_table(self, table_name: str, columns: dict):
+        create_table = """
+CREATE TABLE IF NOT EXISTS {table_name}(
+""".format(table_name=table_name)
+
+        for key in columns.keys():
+            create_table += f"  {key} " + f"{columns[key]},\n"
+
+        create_table = create_table[:-2]
+        create_table += "\n);"
+
+        self.connection.cursor().execute(create_table)
+        self.connection.commit()
+
+    def drop_table(self, table_name: str):
+        self.connection.cursor().execute("DROP TABLE {table_name};".format(table_name=table_name))
+        self.connection.commit()
+
+if __name__ == "__main__":
+    # table_name = "weather_pred"
+    # columns = {
+    #     "month" : "TIMESTAMP NOT NULL",
+    #     "city" : "varchar(20) NOT NULL",
+    #     "state" : "varchar(2) NOT NULL",
+    #     "tempC" : "INT NOT NULL",
+    #     "FeelsLikeC" : "INT NOT NULL",
+    #     "precipMM" : "FLOAT NOT NULL",
+    #     "humidity" : "INT NOT NULL"
+    # }
+
+    # PostgreSQL().drop_table(table_name)
+    # PostgreSQL().create_table(table_name, columns)
