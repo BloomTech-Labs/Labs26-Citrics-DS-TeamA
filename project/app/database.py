50466633bf09b2e86719ae51766846e6c3699d38
Samuel Swank <samswank@gmail.com>
2020-10-08 12:02:44 -0600

    debugged database.py

--- project/app/database.py
+++ project/app/database.py
@@ -3,6 +3,7 @@ import os
 import psycopg2
 from psycopg2.extensions import register_adapter, AsIs
 import pandas as pd
+
 # Load environment variables from .env
 load_dotenv()
 
@@ -12,9 +13,7 @@ class PostgreSQL:
         self.user = os.getenv("DB_USER")
         self.password = os.getenv("DB_PASSWORD")
         self.host = os.getenv("DB_HOST")
-
-    def connection(self):
-        return psycopg2.connect(
+        self.connection = psycopg2.connect(
             dbname=self.name,
             user=self.user,
             password=self.password,
@@ -34,13 +33,35 @@ class PostgreSQL:
     def close(self):
         self.connection.close()
 
-    def fetch_records(self, query):
-        self.excecute(query)
-        response = self.cursor.fetchall()
-        self.close()
+    def create_table(self, table_name: str, columns: dict):
+        create_table = """
+CREATE TABLE IF NOT EXISTS {table_name}(
+""".format(table_name=table_name)
+
+        for key in columns.keys():
+            create_table += f"  {key} " + f"{columns[key]},\n"
+
+        create_table = create_table[:-2]
+        create_table += "\n);"
+
+        self.connection.cursor().execute(create_table)
+        self.connection.commit()
+
+    def drop_table(self, table_name: str):
+        self.connection.cursor().execute("DROP TABLE {table_name};".format(table_name=table_name))
+        self.connection.commit()
+
+    def fetch_query_records(self, query: str):
+        self.connection.cursor().execute(query)
+        response = list(self.connection.cursor().catchall())
+        self.connection.close()
         return response
 
-    def fetch_json(self, query, columns):
-        response = self.fetch_records(query)
+    def fetch_query(self, query: str, columns: list):
+        self.fetch_query_records(query)
         df = pd.DataFrame(response, columns=columns)
-        return df.to_json(orient="records")
+        return df.to_json(orient=records)
+
+    
+
+    
