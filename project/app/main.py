d38c76102a0b64eb099ca3c7c3ff5089b55f5683
Aaron Watkins <61791690+ekselan@users.noreply.github.com>
2020-10-14 13:44:41 -0400

    deploy version 2.6

--- project/app/main.py
+++ project/app/main.py
@@ -26,7 +26,7 @@ DESC_TEXT = "Finding a place to live is hard! Nomads struggle with finding the r
 app = FastAPI(
     title='Citrics API',
     description=DESC_TEXT,
-    version='2.5',
+    version='2.6',
     docs_url='/',
 )
 
