1b1f9633dc3080aed92a34868fd9b96c91c18253
Kenneth T. Barrett <kennethtbarrett@criptext.com>
2020-10-20 16:11:21 -0500

    Mc Edge case

--- project/app/api/static.py
+++ project/app/api/static.py
@@ -64,6 +64,10 @@ async def fetch_static_data(city: str, statecode: str):
     elif city[0:3] == "Ft.":
         city = city.replace("Ft.", "Fort")
 
+    # multiple caps
+    elif city[0:2] == 'Mc':
+        city = city[:2] + city[2:].capitalize()
+    
     # Find matching metro-area in database
     match = df.loc[(df.city.str.contains(city)) &
                    (df.state.str.contains(statecode))]
